e28d762e353b70ea669a9ee3729b7d8c2b0ca329
Claudiordev <58310142+Claudiordev@users.noreply.github.com>
2020-12-06 18:21:38 +0000

    Update Controllers.java

--- src/main/java/claudiordev/springboot/conversions/Controllers.java
+++ src/main/java/claudiordev/springboot/conversions/Controllers.java
@@ -1,24 +1,10 @@
 package claudiordev.springboot.conversions;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
